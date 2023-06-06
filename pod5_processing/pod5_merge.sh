ab96e116adbfa936563db7903f602253cc59771c
Daniel Enriquez <denriquez@tgen.org>
2023-06-06 15:11:35 -0700

    updated variables and singularity module

--- pod5_processing/pod5_merge.sh
+++ pod5_processing/pod5_merge.sh
@@ -7,53 +7,36 @@
 #SBATCH --cpus-per-task 24
 #SBATCH --mem=34G
 
-module load singularity/3.8.4
+module load singularity/3.8.6
 
-pod5_path="$1"
-prefix="$2"
-container="/home/tgenref/containers/pod5-0.1.20.sif"
-binders="/scratch/denriquez,/home/denriquez,/illumina_run_folders"
+PREFIX=$1
+RUNFOLDER_NAME=$2
+PATH_TO_SCRIPTS=$3
+
+source ${PATH_TO_SCRIPTS}/setup.conf
+
+pod5_path=${SEQUENCING_BASE_PATH}/${RUNFOLDER_NAME}/pod5_pass
 output_path=$(dirname ${pod5_path})/merged_pod5
 
 if [ ! -d ${output_path} ]
 then
   mkdir ${output_path} || exit 1
 fi
-
 cd ${pod5_path} || exit 1
 
-# Pick one file to parse for prefix
-# tmp_file=$(ls | grep ".pod5" | head -n 1)
-
-# Get delimiter count
-# delimiter_count=$(echo ${tmp_file} | tr -c -d '_' | wc -c)
-
-# Get prefix
-# if [ ${delimiter_count} -eq 5 ]
-# then
-#     prefix=$(echo ${tmp_file} | cut -d"_" -f1-5)
-# elif [ ${delimiter_count} -eq 4 ]
-# then
-#     prefix=$(echo ${tmp_file} | cut -d"_" -f1-4)
-# else
-#     echo "Error: Underscore delimiter count of ${delimiter_count} is not supported right now!"
-#     exit 1
-# fi
-
-if [ -e ${output_path}/${prefix}.pod5 ]
+if [ -e ${output_path}/${PREFIX}.pod5 ]
 then
-  echo "${output_path}/${prefix}.pod5 Already Exists!"
+  echo "${output_path}/${PREFIX}.pod5 Already Exists!"
   exit 1
 fi
 
-singularity exec --bind ${binders} --pwd $PWD --workdir /tmp --cleanenv --contain --nv ${container} pod5 \
+singularity exec --bind ${BIND} --pwd $PWD --workdir /tmp --cleanenv --contain --nv ${POD5_CONTAINER} pod5 \
 	merge \
 	*.pod5 \
-	${output_path}/${prefix}.pod5
+	${output_path}/${PREFIX}.pod5
 
 if [ $? -eq 0 ]
 then
     cd ${output_path}
-    md5sum ${prefix}.pod5 > ${prefix}.pod5.md5
+    md5sum ${PREFIX}.pod5 > ${PREFIX}.pod5.md5
 fi
-
