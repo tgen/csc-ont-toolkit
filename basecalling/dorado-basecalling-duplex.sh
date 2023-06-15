7f18f1befda793ddbdae77324ce9953b26d458dc
Daniel Enriquez <denriquez@tgen.org>
2023-06-15 14:20:03 -0700

    adjusted compute resources and fixed cuda env vars

--- basecalling/dorado-basecalling-duplex.sh
+++ basecalling/dorado-basecalling-duplex.sh
@@ -4,27 +4,25 @@
 #SBATCH --nodes 1
 #SBATCH --partition=gpu
 #SBATCH -C A100
-#SBATCH --cpus-per-task 32
+#SBATCH --cpus-per-task 20
 #SBATCH --gres gpu:4
 #SBATCH --mem=32G
-#SBATCH -t 0-36:00
+#SBATCH -t 0-48:00
 #SBATCH --job-name="dorado_duplex_basecalling"
 #SBATCH -o dorado_duplex_%j.out
-#SBATCH --profile=ltask
-#SBATCH --acctg-freq=task=1
-
-sbatch -n1 -d$SLURM_JOB_ID --wrap="sh5util -j $SLURM_JOB_ID -o profiling_dorado_duplex_${SLURM_JOB_ID}.h5"
 
 module load singularity/3.8.6
 module load SAMtools/1.10-Container
-export CUDA_MODULE_LOADING=LAZY
+export SINGULARITYENV_CUDA_MODULE_LOADING=LAZY
+export SINGULARITYENV_PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:4096
+
 export SINGULARITYENV_CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}
 
 NAME=$1
 RUNFOLDER_NAME=$2
 PATH_TO_SCRIPTS=$3
 OUTDIR=$4
-THREADS=31
+THREADS=19
 
 source ${PATH_TO_SCRIPTS}/setup.conf
 
@@ -49,10 +47,10 @@ echo "Starting Duplex Basecalling"
 echo
 time singularity exec --bind $BIND --pwd $PWD --workdir /tmp --cleanenv --contain --nv ${DORADO_CONTAINER} dorado \
 	duplex \
-	${PATH_TO_MODEL} \
-	${PATH_TO_POD5} \
 	--threads ${THREADS} \
-	-b ${BATCH_SIZE} > ${NAME}.${DUPLEX_EXTENSION}
+	-v \
+	${PATH_TO_MODEL} \
+	${PATH_TO_POD5}  > ${NAME}.${DUPLEX_EXTENSION}
 
 if [ $? -eq 0 ]
 then
